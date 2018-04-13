6c160cfd69dcb6f1aa7fc6fec81bd526dbf21eeb
atowinram <38089861+atowinram@users.noreply.github.com>
2018-04-13 01:01:54 -0400

    edit

--- src/InstructPanel.java
+++ src/InstructPanel.java
@@ -8,25 +8,16 @@ import java.beans.*;
 public class InstructPanel extends JPanel implements ActionListener
 {
     XML_240 x2;
-<<<<<<< HEAD
-        
-=======
-    XMLDecoder de;
-    String instructions = "";
->>>>>>> e64db2314cd271e627a5418d6fb1d290b676fd64
+      
+
     JButton b1;
     JButton b2;
 
     public InstructPanel()
     {
         super();
-<<<<<<< HEAD
         setBackground(Color.darkGray);
         x2 = new XML_240();
-=======
-        x2 = new XML_240();        
-        setBackground(Color.yellow);
->>>>>>> e64db2314cd271e627a5418d6fb1d290b676fd64
         b1 = new JButton();
         b1.setText("Click Here To View Instructions");
         b1.setBounds(new Rectangle(50,50,150,50));
@@ -36,14 +27,10 @@ public class InstructPanel extends JPanel implements ActionListener
         b2.setBounds(new Rectangle(550,550,150,50));
         b2.addActionListener(this);
         add(b1);
-<<<<<<< HEAD
-        add(b2);        
-=======
         add(b2);
         x2.openInstructions("InstructPanel.xml");
-        instructions = (String) x2.ReadObject();
         x2.closeReaderXML();        
->>>>>>> e64db2314cd271e627a5418d6fb1d290b676fd64
+
     }
     
     public void actionPerformed(ActionEvent event)
@@ -51,7 +38,7 @@ public class InstructPanel extends JPanel implements ActionListener
         Object obj = event.getSource();
         if (obj == b1)
         {
-<<<<<<< HEAD
+
             b1.setVisible(false);
             b2.setVisible(false);
             setBackground(Color.blue);
@@ -64,34 +51,7 @@ public class InstructPanel extends JPanel implements ActionListener
             add(b3);
         }    
             
-=======
-            try
-            {
-                de = new XMLDecoder(new BufferedInputStream(new FileInputStream("InstructPanel.xml")));
-            }
-            catch (Exception xx)
-            {
-                xx.printStackTrace();
-            }
-            try
-            {
-                instructions = (String) de.readObject();
-                b1.setText(instructions);
-            }
-            catch (Exception xx)
-            {
-                xx.printStackTrace();
-            }
-            try
-            {
-                de.close();
-            }
-            catch (Exception xx)
-            {
-                xx.printStackTrace();
-            }
-        }
->>>>>>> e64db2314cd271e627a5418d6fb1d290b676fd64
+
         if (obj == b2)
         {
             JFrame MyJFrame = (JFrame) SwingUtilities.getRoot(this);
