d5e3fa6aebd7af3c6248f9fe5068b211a793abc9
atowinram <38089861+atowinram@users.noreply.github.com>
2018-04-19 22:29:41 -0400

    you lose screen

---
+++ src/YouLose.java
@@ -0,0 +1,46 @@
+/* IST 240 Team 5
+ * Michael Egnak, , 
+ * Thomas Ling
+ * Melissa Scott
+ * Leonard Cadet
+ */
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+// This is a place holder until we know when to use this text screen in our game
+
+public class YouLose extends JPanel implements ActionListener
+{
+    JButton b1;
+    JButton p2;
+
+    public YouLose()
+    {
+        super();
+        setBackground(Color.RED);
+        b1 = new JButton();
+        ImageIcon imageB1 = new ImageIcon("images/YouLose.jpg");
+        b1.setIcon(imageB1);
+        b1.setBounds(new Rectangle(100,50,150,50));
+        add(b1);
+        
+        p2 = new JButton();
+        p2.setText("Click to End Game");
+        p2.setBounds(new Rectangle(550,550,150,50));
+        p2.addActionListener(this);
+        add(p2);
+    }
+    
+    public void actionPerformed(ActionEvent event)
+    {
+        Object obj = event.getSource();           
+
+        if (obj == p2)
+        {
+            System.exit(0); 
+        }
+    }
+}
